c0f28a39582eb08300230391205515d7c2da887d
Dotan Dimet <dotan@corky.net>
2014-12-30 22:06:49 +0200

    new db version

--- mia_net/lib/MiaNet/Controller/CRUD.pm
+++ mia_net/lib/MiaNet/Controller/CRUD.pm
@@ -31,5 +31,20 @@ sub create {
 }
 }
 
+sub fetch {
+  my $self = shift;
+  my $table = $self->stash('table');
+  $self->app->log->debug("Got table name: $table");
+  my $rs = $self->rs($table);
+  if (!$rs) {
+    $self->render(json => { error => "table $table doesn't seem to exist" });
+  }
+  else {
+  my $data = ($self->req->headers->content_type =~ /json/) ? $self->req->json : json_decode $self->param('data');
+  my $result = $rs->create($data);
+  $self->render(json => ($result) ? { success => \1, $table => $result } : { error => 'failed to create object' } );
+}
+}
+
 1;
 
