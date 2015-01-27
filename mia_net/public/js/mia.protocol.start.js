bcfe204ed5e9047b788cb8a5e26ae0d9f9a0cc3f
Dotan Dimet <dotan@corky.net>
2015-01-27 15:54:12 +0200

    protocol start work

--- mia_net/public/js/mia.protocol.start.js
+++ mia_net/public/js/mia.protocol.start.js
@@ -6,11 +6,19 @@ Mia.Protocol.start.controller = function() {
   this.user = Mia.common.user;
   this.start_time = m.prop(new Date);
   this.go = function() {
-    if (!this.user.logged_in()) {
+    if (!this.user().logged_in()) {
       alert('Please Login');
       return false;
     }
-   else
+    else {
+//       if (1) {
+//         m.request({method: 'POST', url: '/Run/create', data: { protocol_id: 1, start_time: this.start_time().getTime() }})
+//           .then(res) {
+//             
+//           }
+         m.route('/protocol/' + this.protocol.id + '/run/1/step/1');
+//       }
+    }
   }.bind(this);
   return this;
 }
@@ -21,7 +29,7 @@ Mia.Protocol.start.view = function(ctrl) {
             m('div.row', [
               m('div.columns.eight', [
           m('h2', protocol.name), m('h3', protocol.description),
-          m('submit', { value: 'Begin'} ),
+          m('input[type=submit]', { value: 'Begin'} ),
       ] ), m('div.columns.two', [
             m('p', [ 'Version:', m('i', protocol.version) ]),
           m('p', [ 'Status:', m('b', protocol.status) ]),
